4bf5d3c3954e31a3a79936e8e38fb2039f3a584d
LaurynLafayette <laurynlafayette@ufl.edu>
2025-07-29 12:15:57 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,7 +5,19 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def square_root(a):
+    try:
+        result = math.sqrt(a)
+        return result
+    except ValueError:
+        raise ValueError("Input value 'a' cannot be negative.")
+
+def hypotenuse(a, b):
+    result = math.hypot(a,b)
+    return result
 
 
+def add(a, b):
+    return a + b
