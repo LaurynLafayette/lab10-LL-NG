7246340f8fab451b957ec4faa693c05321e5ffda
LaurynLafayette <laurynlafayette@ufl.edu>
2025-07-30 22:50:08 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -18,6 +18,26 @@ def hypotenuse(a, b):
     result = math.hypot(a,b)
     return result
 
-
 def add(a, b):
     return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except ZeroDivisionError:
+        raise ZeroDivisionError("Cannot divide by 0")
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError:
+        raise ValueError("Invalid Value")
+
+def exp(a, b):
+    return a ** b
