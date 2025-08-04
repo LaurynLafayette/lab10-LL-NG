d0c8a3b6279b24fdf555e41ae3bd74e775c6598f
nickguedes <nickguedes9@gmail.com>
2025-08-03 21:40:12 -0400

    p1

--- calculator.py
+++ calculator.py
@@ -1,12 +1,27 @@
+# https://github.com/LaurynLafayette/lab10-LL-NG
+# Partner 1: Lauryn Lafayette
+# Partner 2: Nick Guedes
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+# First example
 import math
 
-def add(a, b): 
+def square_root(a):
+    try:
+        result = math.sqrt(a)
+        return result
+    except ValueError:
+        raise ValueError("Input value 'a' cannot be negative.")
+
+def hypotenuse(a, b):
+    result = math.hypot(a,b)
+    return result
+
+def add(a, b):
     return a + b
 
 def sub(a, b):
